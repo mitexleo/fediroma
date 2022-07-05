4833bc927e9f360fb2870ad18e54558b5fc1fe9a
Norm <normandy@biribiri.dev>
2022-07-05 06:01:25 -0400

    Add /etc/akkoma in the config search path
    Keeping /etc/pleroma to ease migration from Pleroma.

--- lib/pleroma/config/release_runtime_provider.ex
+++ lib/pleroma/config/release_runtime_provider.ex
@@ -12,7 +12,8 @@ defmodule Pleroma.Config.ReleaseRuntimeProvider do
     with_defaults = Config.Reader.merge(config, Pleroma.Config.Holder.release_defaults())
 
     config_path =
-      opts[:config_path] || System.get_env("PLEROMA_CONFIG_PATH") || "/etc/pleroma/config.exs"
+      opts[:config_path] || System.get_env("PLEROMA_CONFIG_PATH") || "/etc/akkoma/config.exs" ||
+        "/etc/pleroma/config.exs"
 
     with_runtime_config =
       if File.exists?(config_path) do
