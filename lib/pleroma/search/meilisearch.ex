7cfb2afeec19c375737bdc2e24ec836e028245a9
FloatingGhost <hannah@coffee-and-dreams.uk>
2022-09-20 11:15:34 +0100

    use taskUid to detect success

--- lib/pleroma/search/meilisearch.ex
+++ lib/pleroma/search/meilisearch.ex
@@ -153,7 +153,7 @@ defmodule Pleroma.Search.Meilisearch do
         )
 
       with {:ok, res} <- result,
-           true <- Map.has_key?(res, "indexUid") do
+           true <- Map.has_key?(res, "taskUid") do
         # Do nothing
       else
         _ ->
