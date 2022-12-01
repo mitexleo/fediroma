5bc9abff46a93df8580f7d0b3744b818bd96290f
FloatingGhost <hannah@coffee-and-dreams.uk>
2022-12-01 10:19:01 +0000

    Do not persist remote transient activities

--- priv/repo/migrations/20221129112022_add_cascade_to_report_notes_on_activity_delete.exs
+++ priv/repo/migrations/20221129112022_add_cascade_to_report_notes_on_activity_delete.exs
@@ -2,16 +2,18 @@ defmodule Pleroma.Repo.Migrations.AddCascadeToReportNotesOnActivityDelete do
   use Ecto.Migration
 
   def up do
-    drop constraint(:report_notes, "report_notes_activity_id_fkey")
+    drop(constraint(:report_notes, "report_notes_activity_id_fkey"))
+
     alter table(:report_notes) do
-        modify :activity_id, references(:activities, type: :uuid, on_delete: :delete_all)
+      modify(:activity_id, references(:activities, type: :uuid, on_delete: :delete_all))
     end
   end
 
   def down do
-    drop constraint(:report_notes, "report_notes_activity_id_fkey")
+    drop(constraint(:report_notes, "report_notes_activity_id_fkey"))
+
     alter table(:report_notes) do
-        modify :activity_id, references(:activities, type: :uuid)
+      modify(:activity_id, references(:activities, type: :uuid))
     end
   end
 end
