c38555de5c6a4c23393801977bb5c0fb8baf18dc
Floatingghost <hannah@coffee-and-dreams.uk>
2024-05-29 06:12:23 +0100

    don't require RPC

--- docker-resources/manage.sh
+++ docker-resources/manage.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-docker compose run --rm akkoma ./bin/pleroma_ctl $@
+# this should all be done without needing a running instance
+docker compose run --rm -e "PLEROMA_CTL_RPC_DISABLED=true" akkoma ./bin/pleroma_ctl $@
