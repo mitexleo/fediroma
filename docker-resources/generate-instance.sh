bb37aa3be55405a1b1efb623c5266d7f890a2d47
Floatingghost <hannah@coffee-and-dreams.uk>
2024-06-04 10:21:13 +0100

    put setup file in init.d

--- docker-resources/generate-instance.sh
+++ docker-resources/generate-instance.sh
@@ -5,22 +5,9 @@ set -euo pipefail
 mkdir -p pgdata
 
 # This is sorta special in that we need the generated_config.exs to make it onto the host
-# We can also automate the DB setup here!
 docker compose run \
     --rm \
     -e "PLEROMA_CTL_RPC_DISABLED=true" \
     akkoma ./bin/pleroma_ctl instance gen --no-sql-user --no-db-creation --dbhost db --dbname akkoma --dbuser akkoma --dbpass akkoma --listen-ip 0.0.0.0 --listen-port 4000 --static-dir /opt/akkoma/instance/ --uploads-dir /opt/akkoma/uploads/ --db-configurable true --output /opt/akkoma/config/generated_config.exs --output-psql /opt/akkoma/config/setup_db.psql
 
-echo ""
-echo "=========================="
-echo ""
-echo "Setting up your database!"
-
-docker compose start db
-
-docker compose run \
-    --rm \
-    -e "PGPASSWORD=akkoma" \
-    -v "$(pwd)/config:/docker-entrypoint-initdb.d" \
-    db
-
+echo "Instance generated!"
