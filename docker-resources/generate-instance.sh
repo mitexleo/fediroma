569851f14c78e8b1449566428377d6d2537f0a5c
Floatingghost <hannah@coffee-and-dreams.uk>
2024-06-04 10:27:50 +0100

    fix setup

--- docker-resources/generate-instance.sh
+++ docker-resources/generate-instance.sh
@@ -11,3 +11,11 @@ docker compose run \
     akkoma ./bin/pleroma_ctl instance gen --no-sql-user --no-db-creation --dbhost db --dbname akkoma --dbuser akkoma --dbpass akkoma --listen-ip 0.0.0.0 --listen-port 4000 --static-dir /opt/akkoma/instance/ --uploads-dir /opt/akkoma/uploads/ --db-configurable true --output /opt/akkoma/config/generated_config.exs --output-psql /opt/akkoma/config/setup_db.psql
 
 echo "Instance generated!"
+
+# setup database from generated config
+# we run from the akkoma container to ensure we have the right environment! can't connect to a DB that doesn't exist yet...
+docker compose run \
+    --rm \
+    -e "PLEROMA_CTL_RPC_DISABLED=true" \
+    -e "PGPASSWORD=akkoma" \
+    akkoma psql -h db -U akkoma -d akkoma -f /opt/akkoma/config/setup_db.psql
